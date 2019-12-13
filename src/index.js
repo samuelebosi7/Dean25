69806b252b27c8d02bacbbae9908a2083a2c0a3f
Samu997 <10565756@mail.polimi.it>
2019-12-13 12:30:49 +0100

    rimozione file .js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import './js/statusBar-script.js';
 import './style/style.css';
 import './style/menu-style.css';
 import './style/statusBar-style.scss';
 import '@fortawesome/fontawesome-free/js/all'
-import Vue from 'vue';
 import App from './app.vue'
 
 new Vue({
