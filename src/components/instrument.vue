208debd1242aeea4ef1c5b63bcd03603274abbd5
Francesco <francesco8.castelli@mail.polimi.it>
2019-12-19 11:07:28 +0100

    change instrument.vue

--- src/components/instrument.vue
+++ src/components/instrument.vue
@@ -6,22 +6,22 @@
         <div class="euclidean-input">
             <div class="input-num">
                 <span class="input-label"> STEPS </span> </br>
-                <input v-on:click="decrementValue" type="button" value="-" class="button-minus" data-field="quantity">
+                <input v-on:click="decrementValue(true , $event)" type="button" value="-" class="button-minus" data-field="quantity">
                 <input type="number" step="1" max="" value="1" name="quantity" class="num-value numerator">
-                <input v-on:click="incrementValue" type="button" value="+" class="button-plus" data-field="quantity">
+                <input v-on:click="incrementValue(true , $event)" type="button" value="+" class="button-plus" data-field="quantity">
             </div>
             
             <div class="input-num">
                 <span class="input-label"> PULSES </span> </br>
-                <input v-on:click="decrementValue" type="button" value="-" class="button-minus" data-field="quantity">
+                <input v-on:click="decrementValue(false , $event)" type="button" value="-" class="button-minus" data-field="quantity">
                 <input type="number" step="1" max="" value="1" name="quantity" class="num-value denominator">
-                <input v-on:click="incrementValue" type="button" value="+" class="button-plus" data-field="quantity">
+                <input v-on:click="incrementValue(false , $event)" type="button" value="+" class="button-plus" data-field="quantity">
             </div>
             
             <div class="input-num"><span class="input-label"> OFFSET </span> </br>
-                <input v-on:click="decrementValue" type="button" value="-" class="button-minus" data-field="quantity">
+                <input v-on:click="decrementValue(false , $event)" type="button" value="-" class="button-minus" data-field="quantity">
                 <input type="number" step="1" max="" value="1" name="quantity" class="num-value denominator">
-                <input v-on:click="incrementValue" type="button" value="+" class="button-plus" data-field="quantity">
+                <input v-on:click="incrementValue(false , $event)" type="button" value="+" class="button-plus" data-field="quantity">
             </div>
             <knob>
             </knob>
@@ -54,22 +54,24 @@ export default {
       this.instrumentList.splice(pos, 1);
     },
 
-    incrementValue: function(e) {
+    incrementValue: function(isSet , e) {
         e.preventDefault();
-        var fieldName = $(e.target).data('field');
+        var fieldName = $(event.target).data('field');
         var parent = $(e.target).closest('div');
         var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
     
         if(!isNaN(currentVal) && currentVal <= 15){
             parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
-            this.$store.commit('setStep' , {newStep: currentVal+1 , id: this.id});
+            if(isSet)
+                this.$store.commit('setStep' , {newStep: currentVal+1 , id: this.id});
         }else if(isNaN(currentVal)){
             parent.find('input[name=' + fieldName + ']').val(0);
+            if(isSet)
             this.$store.commit('setStep' ,{newStep: 0 , id: this.id});
         }
     },
 
-    decrementValue: function(e) {
+    decrementValue: function( isSet , e) {
         e.preventDefault();
         var fieldName = $(e.target).data('field');
         var parent = $(e.target).closest('div');
@@ -77,9 +79,11 @@ export default {
     
         if (!isNaN(currentVal) && currentVal > 1) {
             parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
-            this.$store.commit('setStep', {newStep: currentVal-1 , id: this.id});
+            if(isSet)
+                this.$store.commit('setStep', {newStep: currentVal-1 , id: this.id});
         } else {
             parent.find('input[name=' + fieldName + ']').val(1);
+            if(isSet)
             this.$store.commit('setStep' ,{newStep: 1 , id: this.id});
         }
     }
